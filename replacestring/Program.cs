384821e702468f626fec06cb2068af4552b53f22
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 17:53:17 +0530

    Add project files.

---
+++ replacestring/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace replacestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "abcdefg";
+
+            Console.WriteLine("before replacing : " + str);
+
+            Console.WriteLine("after replacing: " + str.Replace('f', 'z'));
+
+        }
+    }
+}
